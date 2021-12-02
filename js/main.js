b61c1b6f9a84dda41687009e548c765ff4306ea5
Fantast1k <83510907+HSFantast1k@users.noreply.github.com>
2021-12-02 21:09:55 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -6,5 +6,23 @@ $(function(){
         prevArrow: '<button type="button" class="slick-prev slick-arrow"><img src="images/arrow-top.svg" alt=""></button>',
         nextArrow: '<button type="button" class="slick-next slick-arrow"><img src="images/arrow-bottom.svg" alt=""></button>',
     });
+    
+    $('.product__name').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        focusOnSelect: true,
+        prevArrow: '<button type="button" class="product-prev product-arrow"><img src="images/product-top.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="product-next product-arrow"><img src="images/product-bottom.svg" alt=""></button>',
+        asNavFor: '.product__content',
+        vertical: true,
+        centerMode: true,
+      });
+      $('.product__content').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: '.product__name',
+        fade: true,
+        arrows: false,  
+      });
 
 });
